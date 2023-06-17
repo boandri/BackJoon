b99bda2e1695e58483d91158e672134d3ecdc0e9
jang ssau <seowoojang05@gmail.com>
2023-06-17 15:09:46 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+# 1271 엄청난부자2
+# n, m = map(float, input().split())
+# print(n//m)
+# print(n%m)
+
+
+#1330 두 수 비교하기
+# A, B = map(int, input().split())
+# if(A>B):
+#     print('>')
+# elif(A<B):
+# 	print('<')
+# elif(A==B):
+# 	print('==')
+
+
+#1809 moo
+# "(___)
+# (o o)____/
+#  @@      \\
+#   \\ ____,/
+#   //   //
+#  ^^   ^^"
