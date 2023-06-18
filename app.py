9f64c78d516cd5370e69cd4fddb34b086d25ec44
jang ssau <seowoojang05@gmail.com>
2023-06-18 00:16:40 -0400

    updated #2438

--- app.py
+++ app.py
@@ -21,3 +21,45 @@
 #   \\ ____,/
 #   //   //
 #  ^^   ^^"
+
+#2338 긴자리계산
+# a = int(input("enter first number:"))
+# b = int(input("enter second number:"))
+
+# def cal(a, b):
+#     print(a+b)
+#     print(a-b)
+#     print(a*b)
+
+# cal(a,b)
+
+#2393 rook
+# print("  ___  ___  ___")
+# print("  | |__| |__| |")
+# print("  |           |")
+# print("   \\_________/")
+# print("    \\_______/")
+# print("     |     |")
+# print("     |     |")
+# print("     |     |")
+# print("     |     |")
+# print("     |_____|")
+# print("  __/       \__")
+# print(" /             \\")
+# print("/_______________\\")
+
+#2438 별찍기
+n = int(input())
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    print("")
+
+
+
+
+
+
+
+
+
