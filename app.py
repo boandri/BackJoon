d70fdf4c0f627abb716a93f22b06b3eac81f876a
jang ssau <seowoojang05@gmail.com>
2023-07-27 14:08:40 -0400

    #9498 update

--- app.py
+++ app.py
@@ -189,3 +189,32 @@
 # print(min(students))
 # print(max(students))
 
+
+
+#8393 합
+# gen = list(range(1,int(input())+1))
+# sum = sum(gen)
+# print(sum)
+
+
+
+#9086 문자열
+# a = list()
+# for i in range(int(input())):
+#     a.append(input())
+# for i in range(len(a)):
+#     print(a[i][0]+a[i][-1])
+
+
+#9498 시험성적
+score = int(input())
+if(score >= 90 and score <= 100):
+    print("A")
+elif(score >= 80 and score <=89 ):
+    print("B")
+elif(score >= 70 and score <=79 ):
+    print("C")
+elif(score >= 60 and score <=69 ):
+    print("D")
+else:
+    print("F")
