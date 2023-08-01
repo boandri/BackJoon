145dbc093f5c60703477b34b195789810c7c74fe
jang ssau <seowoojang05@gmail.com>
2023-07-31 23:41:24 -0400

    10430 update

--- app.py
+++ app.py
@@ -207,14 +207,22 @@
 
 
 #9498 시험성적
-score = int(input())
-if(score >= 90 and score <= 100):
-    print("A")
-elif(score >= 80 and score <=89 ):
-    print("B")
-elif(score >= 70 and score <=79 ):
-    print("C")
-elif(score >= 60 and score <=69 ):
-    print("D")
-else:
-    print("F")
+# score = int(input())
+# if(score >= 90 and score <= 100):
+#     print("A")
+# elif(score >= 80 and score <=89 ):
+#     print("B")
+# elif(score >= 70 and score <=79 ):
+#     print("C")
+# elif(score >= 60 and score <=69 ):
+#     print("D")
+# else:
+#     print("F")
+
+
+#10430 나머지
+a, b, c = map(int, input().split())
+print((a+b)%c)
+print(((a%c) + (b%c))%c)
+print((a * b)%c)
+print(((a%c) * (b%c))%c)
