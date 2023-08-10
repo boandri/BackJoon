95a22415c2c4a53ef9b5616e202c1fa9d32c4f6f
jang ssau <seowoojang05@gmail.com>
2023-08-09 23:52:31 -0400

    14681 updated

--- app.py
+++ app.py
@@ -247,4 +247,18 @@
 
 
 #11654 ascii code
-print(ord(input()))
+# print(ord(input()))
+
+
+
+#14681 사분면 고르기
+a = int(input())
+b = int(input())
+if (a > 0 and b > 0):
+    print(1)
+elif(a>0 and b<0):
+    print(4)
+elif(a<0 and b>0):
+    print(2)
+elif(a<0 and b<0):
+    print(3)
